a5587aaae718d454f76617cb140a47d27af9b897
Vitalii Abetkin <abvit89@gmail.com>
2018-01-12 19:17:40 +0300

    tmp

--- lib/pipeline.ex
+++ lib/pipeline.ex
@@ -1,4 +1,6 @@
-
+defmodule State do
+  # a marker
+end
 
 defmodule Pipeline do
   # require IEx
@@ -8,15 +10,27 @@ defmodule Pipeline do
   """
 
   defstruct [
+    :mod,
     :get_deps, # fn mod -> [] end
     :layers, # []
   ]
 
+  def get_layers(%Pipeline{} = pp, resolved, els) do
+    start = [State]
+    lr0 = for mod <- all_modules do
+      pp.get_deps.(mod)
+    end
+  end
+
   def get_fun_layers(%Pipeline{} = pp, mod) do
+    get_deps = fn
+      State -> []
+      mod -> pp.get_deps.(mod)
+    end
     for layer <- pp.layers do
       for mod <- layer do
         fn state ->
-          args = for dep <- pp.get_deps.(mod),
+          args = for dep <- get_deps.(mod),
             do: state[dep]
           %{mod => apply(mod, :run, args)}
         end
