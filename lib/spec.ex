e8a600a31ac8a0c191b99d7dc53811c22555bf23
pwtail <abvit89@gmail.com>
2018-01-19 01:32:51 +0300

    spec

--- lib/spec.ex
+++ lib/spec.ex
@@ -1,33 +1,32 @@
 
+defmodule Dep do
 
-defmodule M do
-  @moduledoc """
-  
-  requires and exports paths
-  """
-
-  @export """
-  user
-  """
-  
-  exp f, do:
-    {:some, {:path}}
-
-  co f(%{http: %{params: p}}) do
-    # if f :flag, do: {:a, {:b, {:c}}}
-    inspect 
-    
-  end
-
-  def test do
-    req {:http, :params}
-    gql {:user, [a: :b]}
-    # can be in the same namespace
-  end
-
-  dep %{http: params} do
-    def [:some | path] do
+  defmacro __using__(_options) do
+    quote do
+      import Dep
     end
   end
+
+  defmacro dep a, b, c do
+    IO.inspect a
+    IO.puts "---"
+    IO.inspect b
+    IO.puts "---"
+    IO.inspect c
+    nil
+
+  end
+
+end
+
+
+defmodule M do
+  import Dep
+
+  # dep [http: [params: [p: _] = params]] when p
+
+  dep [http: [params: [p: _] = params], do:
+    def name [:some | path] do
+    end
 
 end
