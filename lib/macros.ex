8d28ded7f9fd6ba7616fdfa9aad787ede9cccebd
Vitalii Abetkin <abvit89@gmail.com>
2018-04-16 09:48:22 +0300

    _

--- lib/macros.ex
+++ lib/macros.ex
@@ -2,7 +2,7 @@ defmodule Macros do
 
   defmacro __using__(_) do
     quote do
-      import Kernel, except: [def: 2]
+      # import Kernel, except: [def: 2]
       import Macros
     end
   end
@@ -17,7 +17,7 @@ defmodule Macros do
     {:when, _line, [inject, con]},
     body
   ) do
-    con |> IO.inspect(label: :body)
+    con |> IO.inspect(label: :cond)
     nil
   end
 
@@ -31,4 +31,17 @@ defmodule M do
   def f(3, a, 4), inject(%AComp{x: x} = c, oth, er) when a + x < 5 do
     23
   end
+
+  def f(a, b) do
+    a + b
+  end
+
+  def g, inject(%A{}, %B{}) do
+    a
+  end
 end
+
+
+# pln |> M.f(3, 4, 4)
+M.f(3, 5)
+|> IO.inspect(label: "f")
